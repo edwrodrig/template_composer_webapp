b880c7da4aaa62996f11ffba09de3a7a73cfe6a5
Edwin <edwrodrig@gmail.com>
2020-09-08 18:24:49 -0300

    fixing error_log variable

--- data/services/ws.php
+++ data/services/ws.php
@@ -5,6 +5,6 @@ use tpl_company_tpl\tpl_project_tpl\app\Controller;
 
 include_once(__DIR__ . '/../../vendor/autoload.php');
 
-$_ENV['ERROR_LOG_FILENAME'] = __DIR__ . '/../error_log';
+$_ENV['ERROR_LOG_FILENAME'] = __DIR__ . '/../../var/error_log';
 
 (new Controller())->run();
