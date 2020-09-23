c08a600c6c1608ec2592d6aaee8b1b699289356f
Edwin <edwrodrig@gmail.com>
2020-09-23 09:20:04 -0300

    Commentarior para ingresar a mysql de debian

--- scripts/credentials.sql
+++ scripts/credentials.sql
@@ -1,3 +1,9 @@
+/*
+Para ingresar a mysql en ubuntu
+sudo mysql --defaults-file=/etc/mysql/debian.cnf
+*/
+
+
 CREATE USER 'tpl_company_tpl_tpl_project_tpl_app_user'@'localhost' IDENTIFIED BY 'password';
 GRANT ALL PRIVILEGES ON tpl_company_tpl_tpl_project_tpl.* TO 'tpl_company_tpl_tpl_project_tpl_app_user'@'localhost';
 
